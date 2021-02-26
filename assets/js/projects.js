f404c460bea1d476a8aa1d3dd9e9519df17f898c
Grace Ho <grace.ho@mcroiltools.com>
2021-02-26 16:00:17 -0600

    added animation

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -22,7 +22,7 @@ const classAndFreelanceProjects = {
     fullTitle: "TestThisFor.Me v 2.0",
     avatarPic: "assets/images/IconsAndLogos/ttfm-logo-clr1.svg",
     shortDesc: "Group Project For Class",
-    longDesc: "A collaborative project where a peer based review system for web developers was created. <br><br> This project fills a need for developers who need other developers to test their complete projects or blocks of code to find any unknown bugs or test edge cases.<br><br> Original description of the original project &uarr; <br><br> Version 2.0 is a refactoring of the TestThisFor.Me into the full MERN stack. Handlebars, SQL, and Sequelize were removed and replaced with React.js, MongoDB, and Mongoose. A Chrome extention with screenshot capcity and gamification with badges were added.",
+    longDesc: "<blockquote>A collaborative project where a peer based review system for web developers was created. <br><br> This project fills a need for developers who need other developers to test their complete projects or blocks of code to find any unknown bugs or test edge cases.</blockquote><p>Version 2.0 is a refactoring of the TestThisFor.Me into the full MERN stack.  <br><br>Handlebars, SQL, and Sequelize were removed and replaced with React.js, MongoDB, and Mongoose. Whereas a Chrome extention with screenshot capcity and gamification with badges were added as well.</p>",
     chipsArray: ["MongoDB", "Mongoose", "Node.js", "OAuth.io", "React.js"],
     sourceLink: "https://github.com/rainczech/ttfm-mern",
     deployedLink: "http://www.testthisfor.me"
@@ -35,19 +35,21 @@ const htmlProjectList = document.getElementById("dynamicProjects");
 
 //functions
 const makeProjectAvatars = classAndFreelanceProjects => {
+  let intervalCounter = 0;
   for (const prop in classAndFreelanceProjects) {
     const loopProp = classAndFreelanceProjects[prop];
-
     const avatarListItem =
-      `<li class="collection-item avatar" data-project="${prop}">
+      `<li class="waves-effect collection-item avatar animate__animated animate__fadeInLeft animate__delay-${intervalCounter}s" data-project="${prop}">      
       <img src="${loopProp.avatarPic}" alt="test this for me logo" class="circle">
       <span class="title">${loopProp.fullTitle}</span>
-      <p>${loopProp.shortDesc}</p>
+      <p>${loopProp.shortDesc}</p>      
       </li>`
-
     htmlProjectList.innerHTML += avatarListItem;
+    intervalCounter++;
   }
 };
+
+
 
 htmlProjectList.addEventListener("click", event => {
   const selectedProject = event.target.closest("li");
@@ -73,6 +75,7 @@ htmlProjectList.addEventListener("click", event => {
 
   document.getElementById("cardInnardsHere").innerHTML = projectCardDisplayInnards
 })
+
 
 
 
