d6c7fe7b9e34750ee302557ae3364293f79148e7
Grace Ho <rainczech@gmail.com>
2021-02-28 19:31:03 -0600

    set up for DGC projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -75,5 +75,24 @@ htmlProjectList.addEventListener("click", event => {
   projectHolder.innerHTML = projectCardDisplayInnards; 
 })
 
+
+// event listeners
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: true
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
+
+
+
 // on page load
 makeProjectAvatars(classAndFreelanceProjects);
