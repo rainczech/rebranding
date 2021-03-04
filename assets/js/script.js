7ea5f23a3fbded3f84ec893d046e8a59498ba1e2
Grace Ho <rainczech@gmail.com>
2021-03-03 22:30:42 -0600

    added tooltip and animation to it

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,16 @@ document.addEventListener("scroll", () => {
     lastKnownScrollPosition = newScrollPosition;
   };
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    hoverEnabled: false
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+});
